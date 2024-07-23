38c843769ab8e58eb25e23d2a0d5c00c12d66c1f
Rasika <rasika@appscrip.co>
2024-07-23 18:41:11 +0530

    fix - unable to get metData Values

--- Sources/IsometrikChat/ViewModel/MessageViewModel/ChatsViewModel.swift
+++ Sources/IsometrikChat/ViewModel/MessageViewModel/ChatsViewModel.swift
@@ -296,7 +296,7 @@ public class ChatsViewModel : NSObject ,ObservableObject,AVAudioPlayerDelegate{
             if !mentionedUsers.isEmpty {
                 body["mentionedUsers"] = mentionedUsers
             }
-        case .location, .contact:
+        case .location, .contact,.post:
             body["metaData"] = metaData
         case .photo, .video:
             body["attachments"] = [attachmentValue]
