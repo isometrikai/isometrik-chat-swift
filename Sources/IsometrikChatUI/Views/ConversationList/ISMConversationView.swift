ec7fc46175213152a2298698b6d56ab860176109
Rasika <rasika@appscrip.co>
2024-07-26 11:39:30 +0530

    fix - added groupName in delegate to display.

--- Sources/IsometrikChatUI/Views/ConversationList/ISMConversationView.swift
+++ Sources/IsometrikChatUI/Views/ConversationList/ISMConversationView.swift
@@ -12,7 +12,7 @@ import ISMSwiftCall
 import IsometrikChat
 
 public protocol ISMConversationViewDelegate{
-    func navigateToMessageList(selectedUserToNavigate : UserDB?,conversationId : String?,isGroup : Bool?,groupImage : String?)
+    func navigateToMessageList(selectedUserToNavigate : UserDB?,conversationId : String?,isGroup : Bool?,groupImage : String?,groupName : String?)
     func navigateToUsersListToCreateChat(conversationType : ISMChatConversationTypeConfig)
 }
 
@@ -100,7 +100,7 @@ public struct ISMConversationView : View {
                             ForEach(realmManager.getConversation()){ data in
                                 if ISMChatSdk.getInstance().getFramework() == .UIKit{
                                     Button {
-                                        delegate?.navigateToMessageList(selectedUserToNavigate: data.opponentDetails, conversationId: data.lastMessageDetails?.conversationId, isGroup: data.isGroup, groupImage: data.conversationImageUrl)
+                                        delegate?.navigateToMessageList(selectedUserToNavigate: data.opponentDetails, conversationId: data.lastMessageDetails?.conversationId, isGroup: data.isGroup, groupImage: data.conversationImageUrl,groupName: data.conversationTitle)
                                     } label: {
                                         ISMConversationSubView(chat: data, hasUnreadCount: (data.unreadMessagesCount) > 0)
                                             .onAppear {
@@ -190,7 +190,7 @@ public struct ISMConversationView : View {
                     if ISMChatSdk.getInstance().getFramework() == .SwiftUI{
                         navigatetoSelectedUser = true
                     }else{
-                        self.delegate?.navigateToMessageList(selectedUserToNavigate: selectedUserToNavigate, conversationId: selectedUserConversationId, isGroup: false, groupImage: nil)
+                        self.delegate?.navigateToMessageList(selectedUserToNavigate: selectedUserToNavigate, conversationId: selectedUserConversationId, isGroup: false, groupImage: nil,groupName: nil)
                     }
                 })
                 .confirmationDialog("", isPresented: $showDeleteOptions) {
