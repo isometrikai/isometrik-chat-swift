ca2c9dd25d1ce53eaeb1acf191fadf7e2cf221b2
Rasiika Bharati <rasika@appscrip.co>
2024-08-09 13:51:32 +0530

    fix - on click of user Profile navigate to userProfile

--- Sources/IsometrikChatUI/Views/MessagesList/ISMMessageView+Buttons.swift
+++ Sources/IsometrikChatUI/Views/MessagesList/ISMMessageView+Buttons.swift
@@ -154,7 +154,7 @@ extension ISMMessageView{
                                     //group profile will not be there in uikit apps
                                     navigateToProfile = true
                                 }else{
-                                    delegate?.navigateToAppProfile(appUserId: self.conversationDetail?.conversationDetails?.metaData?.memberIdOfApp ?? "")
+                                    delegate?.navigateToAppProfile(appUserId: self.opponenDetail?.metaData?.memberIdOfApp ?? "")
                                 }
                             }else{
                                 navigateToProfile = true
