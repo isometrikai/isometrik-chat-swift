ac1a59e920b04475b29a621cdb009244239776b2
Rasiika Bharati <rasika@appscrip.co>
2024-10-04 11:55:03 +0530

    fix - read indicator and api manager

--- Sources/IsometrikChat/Services/APIManager/ISMChatAPIManager.swift
+++ Sources/IsometrikChat/Services/APIManager/ISMChatAPIManager.swift
@@ -76,17 +76,16 @@ extension ISMChatURLConvertible {
  struct ISMChatNewAPIManager {
      
      static func sendRequest<T: Codable, R: Any>(request: ISMChatAPIRequest<R>, showLoader: Bool = true, completion: @escaping (_ result: ISMChatResult<T, ISMChatNewAPIError>) -> Void) {
-
+         
          if showLoader {
              DispatchQueue.main.async {
                  // ISMShowLoader.shared.startLoading()
              }
          }
-
-         // Construct the URL
+         
          var urlComponents = URLComponents(url: request.endPoint.baseURL.appendingPathComponent(request.endPoint.path), resolvingAgainstBaseURL: true)
          urlComponents?.queryItems = request.endPoint.queryParams?.map { URLQueryItem(name: $0.key, value: $0.value) }
-
+         
          guard let url = urlComponents?.url else {
              DispatchQueue.main.async {
                  completion(.failure(.invalidResponse))
@@ -94,89 +93,183 @@ extension ISMChatURLConvertible {
              }
              return
          }
-         print("Request URL: \(url)")
-
-         // Setup headers
-         var headers: HTTPHeaders = [
-             "Content-Type": "application/json",
-             "Connection": "close" // Using 'close' to avoid keep-alive issues
-         ]
-         if let customHeaders = request.endPoint.headers {
-             customHeaders.forEach { key, value in
-                 headers.add(name: key, value: value)
-             }
+         print(url)
+         var urlRequest = URLRequest(url: url)
+         urlRequest.httpMethod = request.endPoint.method.rawValue
+         urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+         urlRequest.setValue("keep-alive", forHTTPHeaderField: "Connection")
+         urlRequest.timeoutInterval = 60
+         
+         // Set headers if provided
+         request.endPoint.headers?.forEach { key, value in
+             urlRequest.setValue(value, forHTTPHeaderField: key)
          }
-
-         // Setup request body if provided
-         var parameters: [String: Any]? = nil
+         
          if let requestBody = request.requestBody as? [String: Any] {
-             parameters = requestBody
-             print("Request Body: \(requestBody)")
-         }
-
-         // Alamofire request
-         
-         AF.request(url, method: HTTPMethod(rawValue: request.endPoint.method.rawValue), parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseDecodable { (response: DataResponse<T, AFError>) in
              do {
-                 if let jsonData = try JSONSerialization.jsonObject(with: response.data ?? Data(), options: .mutableContainers) as? NSDictionary{
-                     ISMChatHelper.print("JSON DATA:",jsonData)
+                 // Serialize dictionary to JSON data
+                 let jsonBody = try JSONSerialization.data(withJSONObject: requestBody, options: [])
+                 urlRequest.httpBody = jsonBody
+                 // Optionally, log the JSON body to ensure it's correct
+                 print("Request Body: \(String(data: jsonBody, encoding: .utf8) ?? "Unable to encode body")")
+             } catch {
+                 DispatchQueue.main.async {
+                     completion(.failure(.invalidResponse))
+                     // ISMShowLoader.shared.stopLoading()
                  }
-             } catch let error {
-                 ISMChatHelper.print("error json serialization :",error)
+                 return
              }
-             guard let statusCode = response.response?.statusCode,let data = response.data else{
-                 return completion(.failure(.none))
-             }
-             switch ISMChat_HTTPStatusCode.init(rawValue: statusCode){
-             case .Success,.Created:
-
-                 do{
-                     let model = try JSONDecoder().decode(T.self, from:data )
-                     completion(.success(model, nil))
-                 }catch let error{
-                     ISMChatHelper.print(error)
-                     let errorData = ISMChat_ErrorData.init(message: error.localizedDescription, error: "Error")
-                     completion(.failure(.none))
-                 }
-             case .UnprocessableEntity :
-//                 if handleUnprocessableEntity {
-//                     do{
-//                         let model = try JSONDecoder().decode(T.self, from:data )
-//                         completion(.success(model))
-//                     }catch let error{
-//                         ISMChatHelper.print("\(error.localizedDescription)")
-//                         let errorData = ISMChat_ErrorData.init(message: error.localizedDescription, error: "Error")
-//                         completion(.failure(errorData))
-//                     }
-//                 }else{
-                     do{
-                         let errorData = try JSONDecoder().decode(ISMChat_ErrorData.self, from:data )
-                         completion(.failure(.none))
-                     }catch let error{
-                         let errorData = ISMChat_ErrorData.init(message: error.localizedDescription, error: "Error")
-                         completion(.failure(.none))
-                     }
-//                 }
-             default :
-                 do{
-                     let errorData = try JSONDecoder().decode(ISMChat_ErrorData.self, from:data )
-                     completion(.failure(.none))
-                 }catch let error{
-                     let errorData = ISMChat_ErrorData.init(message: error.localizedDescription, error: "Error")
-                     completion(.failure(.none))
-                 }
-             }
- //            if isShowLoader{
- //                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()){
- //                    ISMChat_Helper.hideLoader()
- //                }
- //
- //            }
          }
          
-
-             
+         let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+             DispatchQueue.main.async {
+                 guard let httpResponse = response as? HTTPURLResponse else {
+                     completion(.failure(.invalidResponse))
+                     // ISMShowLoader.shared.stopLoading()
+                     return
+                 }
+                 
+                 if let error = error {
+                     completion(.failure(.decodingError(error)))
+                     return
+                 }
+                 
+                 guard let data = data else {
+                     completion(.failure(.invalidResponse))
+                     return
+                 }
+                 
+                 print(JSON(data))
+                 
+                 switch httpResponse.statusCode {
+                 case 200,201:
+                     do {
+                         let responseObject = try JSONDecoder().decode(T.self, from: data)
+                         completion(.success(responseObject, nil))
+                     } catch {
+                         completion(.failure(.decodingError(error)))
+                     }
+                 case 404:
+                     completion(.failure(.httpError(httpResponse.statusCode)))
+                 case 401, 406:
+                     // Handle the refresh token here.
+                     break
+                 default:
+                     completion(.failure(.httpError(httpResponse.statusCode)))
+                 }
+                 
+                 if showLoader {
+                     // ISMShowLoader.shared.stopLoading()
+                 }
+             }
+         }
+         
+         task.resume()
      }
+ }
+     
+//     static func sendRequest<T: Codable, R: Any>(request: ISMChatAPIRequest<R>, showLoader: Bool = true, completion: @escaping (_ result: ISMChatResult<T, ISMChatNewAPIError>) -> Void) {
+//
+//         if showLoader {
+//             DispatchQueue.main.async {
+//                 // ISMShowLoader.shared.startLoading()
+//             }
+//         }
+//
+//         // Construct the URL
+//         var urlComponents = URLComponents(url: request.endPoint.baseURL.appendingPathComponent(request.endPoint.path), resolvingAgainstBaseURL: true)
+//         urlComponents?.queryItems = request.endPoint.queryParams?.map { URLQueryItem(name: $0.key, value: $0.value) }
+//
+//         guard let url = urlComponents?.url else {
+//             DispatchQueue.main.async {
+//                 completion(.failure(.invalidResponse))
+//                 // ISMShowLoader.shared.stopLoading()
+//             }
+//             return
+//         }
+//         print("Request URL: \(url)")
+//
+//         // Setup headers
+//         var headers: HTTPHeaders = [
+//             "Content-Type": "application/json",
+//             "Connection": "close" // Using 'close' to avoid keep-alive issues
+//         ]
+//         if let customHeaders = request.endPoint.headers {
+//             customHeaders.forEach { key, value in
+//                 headers.add(name: key, value: value)
+//             }
+//         }
+//
+//         // Setup request body if provided
+//         var parameters: [String: Any]? = nil
+//         if let requestBody = request.requestBody as? [String: Any] {
+//             parameters = requestBody
+//             print("Request Body: \(requestBody)")
+//         }
+//
+//         // Alamofire request
+//         
+//         AF.request(url, method: HTTPMethod(rawValue: request.endPoint.method.rawValue), parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseDecodable { (response: DataResponse<T, AFError>) in
+//             do {
+//                 if let jsonData = try JSONSerialization.jsonObject(with: response.data ?? Data(), options: .mutableContainers) as? NSDictionary{
+//                     ISMChatHelper.print("JSON DATA:",jsonData)
+//                 }
+//             } catch let error {
+//                 ISMChatHelper.print("error json serialization :",error)
+//             }
+//             guard let statusCode = response.response?.statusCode,let data = response.data else{
+//                 return completion(.failure(.none))
+//             }
+//             switch ISMChat_HTTPStatusCode.init(rawValue: statusCode){
+//             case .Success,.Created:
+//
+//                 do{
+//                     let model = try JSONDecoder().decode(T.self, from:data )
+//                     completion(.success(model, nil))
+//                 }catch let error{
+//                     ISMChatHelper.print(error)
+//                     let errorData = ISMChat_ErrorData.init(message: error.localizedDescription, error: "Error")
+//                     completion(.failure(.none))
+//                 }
+//             case .UnprocessableEntity :
+////                 if handleUnprocessableEntity {
+////                     do{
+////                         let model = try JSONDecoder().decode(T.self, from:data )
+////                         completion(.success(model))
+////                     }catch let error{
+////                         ISMChatHelper.print("\(error.localizedDescription)")
+////                         let errorData = ISMChat_ErrorData.init(message: error.localizedDescription, error: "Error")
+////                         completion(.failure(errorData))
+////                     }
+////                 }else{
+//                     do{
+//                         let errorData = try JSONDecoder().decode(ISMChat_ErrorData.self, from:data )
+//                         completion(.failure(.none))
+//                     }catch let error{
+//                         let errorData = ISMChat_ErrorData.init(message: error.localizedDescription, error: "Error")
+//                         completion(.failure(.none))
+//                     }
+////                 }
+//             default :
+//                 do{
+//                     let errorData = try JSONDecoder().decode(ISMChat_ErrorData.self, from:data )
+//                     completion(.failure(.none))
+//                 }catch let error{
+//                     let errorData = ISMChat_ErrorData.init(message: error.localizedDescription, error: "Error")
+//                     completion(.failure(.none))
+//                 }
+//             }
+// //            if isShowLoader{
+// //                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()){
+// //                    ISMChat_Helper.hideLoader()
+// //                }
+// //
+// //            }
+//         }
+//         
+//
+//             
+//     }
 //     static func sendRequest<T: Codable, R: Any>(request: ISMChatAPIRequest<R>, showLoader: Bool = true, completion: @escaping (_ result: ISMChatResult<T, ISMChatNewAPIError>) -> Void) {
 
 //         if showLoader {
@@ -273,7 +366,7 @@ extension ISMChatURLConvertible {
 //     }
 
  
-}
+//}
 
 
 public enum ISMChatNewAPIError: Error {
