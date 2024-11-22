b6fbbd5592f58b59bf6b3d3d0d2b0935fe3f7634
Rasiika Bharati <rasika@appscrip.co>
2024-11-22 18:28:10 +0530

    fix - issue in conversationlist

--- Sources/IsometrikChat/MQTT/Events/ISMChatMessageDelivered.swift
+++ Sources/IsometrikChat/MQTT/Events/ISMChatMessageDelivered.swift
@@ -45,6 +45,45 @@ public struct ISMChatMessageDelivered: Codable {
     public let mentionedUsers : [ISMChatMentionedUser]?
     public let reactions : [String : [String]]?
     public let meetingId : String?
+    init(sentAt: Double? = nil, messageId: String? = nil, body: String? = nil, senderIdentifier: String? = nil, senderProfileImageUrl: String? = nil, conversationId: String? = nil, updatedAt: Double? = nil, senderName: String? = nil, senderId: String? = nil, metaData: ISMChatMetaData? = nil, customType: String? = nil, userProfileImageUrl: String? = nil, userName: String? = nil, userIdentifier: String? = nil, userId: String? = nil, privateOneToOne: Bool? = nil, messageIds: [String]? = nil, action: String? = nil, attachments: [ISMChatAttachment]? = nil, parentMessageId: String? = nil, senderInfo: ISMChatSenderInfo? = nil, notificationBody: String? = nil, memberName: String? = nil, memberProfileImageUrl: String? = nil, memberIdentifier: String? = nil, memberId: String? = nil, initiatorProfileImageUrl: String? = nil, initiatorName: String? = nil, initiatorIdentifier: String? = nil, initiatorId: String? = nil, conversationTitle: String? = nil, conversationImageUrl: String? = nil, members: [ISMChatMembers]? = nil, details: ISMChatUpdateMessageDetail? = nil, mentionedUsers: [ISMChatMentionedUser]? = nil, reactions: [String : [String]]? = nil, meetingId: String? = nil) {
+        self.sentAt = sentAt
+        self.messageId = messageId
+        self.body = body
+        self.senderIdentifier = senderIdentifier
+        self.senderProfileImageUrl = senderProfileImageUrl
+        self.conversationId = conversationId
+        self.updatedAt = updatedAt
+        self.senderName = senderName
+        self.senderId = senderId
+        self.metaData = metaData
+        self.customType = customType
+        self.userProfileImageUrl = userProfileImageUrl
+        self.userName = userName
+        self.userIdentifier = userIdentifier
+        self.userId = userId
+        self.privateOneToOne = privateOneToOne
+        self.messageIds = messageIds
+        self.action = action
+        self.attachments = attachments
+        self.parentMessageId = parentMessageId
+        self.senderInfo = senderInfo
+        self.notificationBody = notificationBody
+        self.memberName = memberName
+        self.memberProfileImageUrl = memberProfileImageUrl
+        self.memberIdentifier = memberIdentifier
+        self.memberId = memberId
+        self.initiatorProfileImageUrl = initiatorProfileImageUrl
+        self.initiatorName = initiatorName
+        self.initiatorIdentifier = initiatorIdentifier
+        self.initiatorId = initiatorId
+        self.conversationTitle = conversationTitle
+        self.conversationImageUrl = conversationImageUrl
+        self.members = members
+        self.details = details
+        self.mentionedUsers = mentionedUsers
+        self.reactions = reactions
+        self.meetingId = meetingId
+    }
 }
 
 
