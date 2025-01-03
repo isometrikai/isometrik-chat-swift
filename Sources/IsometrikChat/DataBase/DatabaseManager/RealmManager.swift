f8bca8b6ceed008ad2906aa20022443adfa01797
Rasiika Bharati <rasika@appscrip.co>
2025-01-03 17:12:10 +0530

    feat - incraesed version for realm

--- Sources/IsometrikChat/DataBase/DatabaseManager/RealmManager.swift
+++ Sources/IsometrikChat/DataBase/DatabaseManager/RealmManager.swift
@@ -39,7 +39,7 @@ public class RealmManager: ObservableObject {
         do {
             let config = Realm.Configuration(
                 fileURL: getRealmFileURL(for: userId),
-                schemaVersion: 44
+                schemaVersion: 45
             )
             Realm.Configuration.defaultConfiguration = config
             localRealm = try Realm()
