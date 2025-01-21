5769e74fe837264df738fa6e592ee8b97c1a1349
Rasiika Bharati <rasika@appscrip.co>
2025-01-21 12:57:36 +0530

    fix - accept invite

--- Sources/IsometrikChatUI/Views/MessagesList/ISMMessageView.swift
+++ Sources/IsometrikChatUI/Views/MessagesList/ISMMessageView.swift
@@ -488,7 +488,7 @@ public struct ISMMessageView: View {
                             paymentRequestUserId: self.myAppUserId ?? ""
                         )
                     }else{
-                        self.delegate?.dineInInvite(messageId: declinePaymentRequest.messageId ?? "", groupcastId: declinePaymentRequest.metaData?.groupCastId ?? "", reason: "", createdByUserId: declinePaymentRequest.senderInfo?.userIdentifier ?? "", declineByUserId: self.myAppUserId ?? "",inviteStatus: 1)
+                        self.delegate?.dineInInvite(messageId: viewDetailsForPaymentRequest.messageId ?? "", groupcastId: viewDetailsForPaymentRequest.metaData?.groupCastId ?? "", reason: "", createdByUserId: viewDetailsForPaymentRequest.senderInfo?.userIdentifier ?? "", declineByUserId: self.myAppUserId ?? "",inviteStatus: 1)
                     }
                     viewDetailsForPaymentRequest = MessagesDB()
 //                }
