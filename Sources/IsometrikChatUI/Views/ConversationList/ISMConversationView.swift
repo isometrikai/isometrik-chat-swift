bc407f7303bc946d97bf086a158b837bbd130d2b
Rasiika Bharati <rasika@appscrip.co>
2025-11-27 14:22:37 +0530

    fix - userId of loggedin user is empty

--- Sources/IsometrikChatUI/Views/ConversationList/ISMConversationView.swift
+++ Sources/IsometrikChatUI/Views/ConversationList/ISMConversationView.swift
@@ -424,7 +424,7 @@ public struct ISMConversationView : View {
             conversationViewModel: viewModel,
             conversationID: data.lastMessageDetails?.conversationId,
             opponenDetail: data.opponentDetails,
-            myUserId: viewModel.userData?.userId ?? "",
+            myUserId: myUserData?.userId ?? "",
             isGroup: data.isGroup,
             fromBroadCastFlow: false,
             groupCastId: "",
